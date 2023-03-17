79ab087e7cbb02c18c67766e66bd14cc023a8326
Saveli <tuponogov.90@mail.ru>
2023-03-17 20:57:23 +0300

    Обратный отсчет времени с помощью JS

--- src/main/resources/static/js/startGame.js
+++ src/main/resources/static/js/startGame.js
@@ -1,18 +1,31 @@
-const start_btn = document.querySelector(".start_btn button");
-const info_box = document.querySelector(".info_box");
-const exit_btn = info_box.querySelector(".buttons .quit");
-const continue_btn = info_box.querySelector(".buttons .restart ");
-const quiz_box = document.querySelector(".quiz_box");
-// If Start Quiz button click
-start_btn.onclick = ()=>{
-    info_box.classList.add("activeInfo"); //show info box
+const timeCount = document.querySelector(".timer .timer_sec");
+const timeText = document.querySelector(".timer .time_left_txt");
+const option_list = document.querySelector(".option_list");
+const section_list = document.querySelector("section");
+
+let couter;
+
+function startTimer(time){
+    couter = setInterval(timer, 1000);
+    function timer(){
+        console.log("Checks");
+        timeCount.textContent = time;
+        time--;
+        if(time < 9){
+            let addZero = timeCount.textContent;
+            timeCount.textContent = "0" + addZero;
+        }
+        if(time < 0){
+            clearInterval(couter);
+            timeText.textContent = "Time off";
+            const allOptions = option_list.children.length;
+            for(let i=0; i < allOptions; i++){
+                console.log(option_list.children[i]);
+                option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+                section_list.classList.add("disabled");
+                console.log(option_list);
+            }
+        }
+    }
 }
-// if exitQuiz button clicked
-exit_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo"); //hide info box
-}
-// if continueQuiz button clicked
-continue_btn.onclick = ()=> {
-    info_box.classList.remove("activeInfo"); //hide info box
-    quiz_box.classList.add("activeQuiz"); //show quiz box
-}
+startTimer(15);
